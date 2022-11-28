e7ccd69cf853596b37049d40396768b0573569af
abuenocr21dw@ikzubirimanteo.com <abuenocr21dw@ikzubirimanteo.com>
2022-11-28 09:52:46 +0100

    corregido fallo blade crear tema

--- resources/views/temas/index.blade.php
+++ resources/views/temas/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <h1>Introduce tema</h1>
-    <form class=form-group action='{{route("temas-store")}}' method='post>
+    <form class=form-group action='{{route("tema-store")}}' method='post'>
         <input class="lightRounded" type="texto" name="tema" placeholder="Tema" ><br>
         <input type="submit" class = 'greyBlueBtn' value="+ Añadir Tema"> 
     </form>
